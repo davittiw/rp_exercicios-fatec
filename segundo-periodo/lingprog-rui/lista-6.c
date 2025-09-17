b822e5ac751cfba34e497569a57d9f364a58f3de
Luccas Davi <lucctyte@gmail.com>
2025-09-17 11:16:55 -0300

    exercício 2 da lista 6

--- segundo-periodo/lingprog-rui/lista-6.c
+++ segundo-periodo/lingprog-rui/lista-6.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ex05
+#define ex02
 
 #ifdef ex01
 int calculaData(int m, int a, int d)
@@ -103,7 +103,69 @@ int main()
 
 #ifdef ex02
 int main() {
+    int diaN, mesN, anoN;
+    int diaA, mesA, anoA;
 
+    char respostaFinal = 'Y';
+
+    while (respostaFinal != 'N' && respostaFinal != 'n') {
+    // Data atual do usuário
+    printf("Digite sua data de nascimento: ");
+    scanf("%d %d %d", &diaN, &mesN, &anoN);
+
+    // Verificação se o mês está sendo inserido corretamente
+    if(mesN > 12 || mesN < 1 || (mesN == 2 && diaN > 28)) {
+        printf("\nAtencao!\nDigite o mes corretamente: ");
+        scanf("%d", &mesN);
+    }
+
+    // Data atual
+    printf("\n\nAgora, Digite a data ATUAL: ");
+    scanf("%d %d %d", &diaA, &mesA, &anoA);
+
+    // Verificação se o mês está sendo inserido corretamente
+    if(mesA > 12 || mesA < 1 || (mesA == 2 && diaA > 28)) {
+        printf("\nAtencao!\nDigite o mes corretamente: ");
+        scanf("%d", &mesA);
+    }
+
+    // Calcula idade em anos, meses e dias.
+    int meses = mesA - mesN;
+    int dias = diaA - diaN;
+    int qtAnos = anoA - anoN;
+
+    if(diaA < diaN) {
+        dias = -dias;
+    }
+
+    if (dias < 0) {
+        meses--;
+    }
+
+    if (meses < 0) {
+        qtAnos--;
+        meses += 12;
+    }
+    
+    printf("%d anos, %d meses e %d dias.", qtAnos, meses, dias);
+
+    // Pergunta se o usuário deseja continuar o programa
+        printf("\n\nDeseja continuar o programa?\n");
+        printf("'Y' para sim e 'N' para nao: ");
+        scanf(" %c", &respostaFinal);
+
+        // Verificação da resposta
+        while (respostaFinal != 'Y' && respostaFinal != 'y' && respostaFinal != 'N' && respostaFinal != 'n')
+        {
+            printf("Digite um valor valido!\n");
+            printf("Digite 'Y' para sim e 'N' para nao: ");
+            scanf(" %c", &respostaFinal);
+        }
+        if (respostaFinal == 'Y' || respostaFinal == 'y')
+        {
+            system("cls");
+        }
+    }
 }
 #endif // ex02
 
